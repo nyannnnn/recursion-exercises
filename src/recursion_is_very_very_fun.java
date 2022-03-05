b60d17e1412eff9907fc325101f438454065c7f8
nyannnnn <90939473+nyannnnn@users.noreply.github.com>
2022-03-05 10:13:42 -0500

    Update recursion_is_very_very_fun.java

--- src/recursion_is_very_very_fun.java
+++ src/recursion_is_very_very_fun.java
@@ -29,7 +29,7 @@ public class recursion_is_very_very_fun {
 			return x;
 		}
 		if (y < 0) {
-			return -(multiply(x,Math.abs(y)));
+			return -(multiply(x, Math.abs(y)));
 		}
 		return x + multiply(x, Math.abs(y - 1));
 	}
@@ -37,48 +37,63 @@ public class recursion_is_very_very_fun {
 	public static int findVowels(String s) {
 
 		s = s.toLowerCase();
-		int count = 0;
 
 		if (s.length() >= 1) {
 			if (s.charAt(0) == 'a' || s.charAt(0) == 'e' || s.charAt(0) == 'i' || s.charAt(0) == 'o'
 					|| s.charAt(0) == 'u') {
-				count++;
+				return 1 + findVowels(s.substring(1));
 			}
 		} else {
 			return 0;
 		}
 
-		return count + findVowels(s.substring(1));
+		return findVowels(s.substring(1));
 
 	}
 
 	public static String commas(int n) {
 		if (Math.abs(n / 1000) < 1 && n > 0)
 			return "+" + n;
-		else if (Math.abs(n / 1000) < 1|| n == 0)
+		else if (Math.abs(n / 1000) < 1 || n == 0)
 			return "" + n;
-		if(n%1000 == 0) {
-			return commas(n/1000) + ",000";
+		if (n % 1000 == 0) {
+			return commas(n / 1000) + ",000";
 		}
 		return commas(n / 1000) + "," + Math.abs(n % 1000);
 	}
 
 	public static String insert(String s, String ins) {
-				
-		if(s.length() > 1) {
-			if(Character.isLetter(s.charAt(0)) && Character.isLetter(s.charAt(1)) && s.toLowerCase().charAt(0) == s.toLowerCase().charAt(1)) {
-				return s.charAt(0) + ins + insert(s.substring(1), ins);
+		if (s.length() == 1) {
+			if (!Character.isLetter(s.charAt(0))) {
+				return "";
 			}
-			else {
-				return ""+s.charAt(0)+insert(s.substring(1), ins);
-			}
-		}
-		else {
+
 			return s;
 		}
-				
+
+		if (Character.isLetter(s.charAt(0)) && Character.isLetter(s.charAt(1))
+				&& s.toLowerCase().charAt(0) == s.toLowerCase().charAt(1)) {
+			return s.charAt(0) + ins + insert(s.substring(1), ins);
+		} else if (!((Character.isLetter(s.charAt(0))))) {
+			return insert(s.substring(1), ins);
+		} else if (!(Character.isLetter(s.charAt(1)))) {
+			return insert(("" + s.charAt(0) + s.substring(2)), ins);
+		} else {
+			return "" + s.charAt(0) + insert(s.substring(1), ins);
+		}
+
 	}
 	
+	public static int min(int [] num, int index) {
+		if(index == 0) {
+			return num[0];
+		}
+		
+		if(num[0] > num[1]) {
+			return 
+		}
+	}
+
 	public static void main(String[] args) {
 
 		System.out.println(sumReciprocal(5));
@@ -86,7 +101,8 @@ public class recursion_is_very_very_fun {
 		System.out.println(multiply(-8, -7));
 		System.out.println(findVowels("AWEsome"));
 		System.out.println(commas(12345656));
-		System.out.println(insert("aaAA", "!"));
+		System.out.println(insert("@@@@@@@", "@"));
+		System.out.println(min({23, 5, -2, 2000, 234}, 0));
 
 	}
 
